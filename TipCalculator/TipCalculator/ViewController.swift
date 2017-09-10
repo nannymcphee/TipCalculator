cf5baf48999da548c01422da41cbd07e46fa50ec
Nguyên Duy <victornguyen0705@gmail.com>
2017-09-10 15:50:31 +0700

    add button

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var x = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
